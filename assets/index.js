0ddd557610fcab57930119d969ad76fd1176834e
Sebastien Gau <gausebastien@hotmail.fr>
2023-04-20 17:36:46 +0200

    Intégration complète du carroussel sans bootstrap.bundle.js
    Correction de media queries sur la taille des images
    affichées dans les modales
    
    Suppression de code CSS inutile dans bootstrap.css
    
    Mise à jour des balises de référencement local

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,154 @@
+// Variables globales
+let compteur = 0; // Compteur qui permet de connaître l'image sur laquelle on se trouve
+let timer, elements, slides, slideWidth, speed, transition;
+
+window.onload = () => {
+    // On récupère le diaporama
+    const diapo = document.querySelector(".diapo");
+    // On récupère le data-speed
+    speed = diapo.dataset.speed;
+    transition = diapo.dataset.transition;
+
+    elements = document.querySelector(".elements");
+
+    // On clone la 1ère image
+    let firstImage = elements.firstElementChild.cloneNode(true);
+
+    // On injecte le clone à la fin du diapo
+    elements.appendChild(firstImage);
+
+    slides = Array.from(elements.children);
+
+    // On récupère la largeur d'une slide
+    slideWidth = diapo.getBoundingClientRect().width;
+
+    // On récupère les flèches
+    let next = document.querySelector(".carousel-control-next");
+    let prev = document.querySelector(".carousel-control-prev");
+
+    // On gère le clic
+    next.addEventListener("click", slideNext);
+    prev.addEventListener("click", slidePrev);
+
+    // On automatise le défilement
+    timer = setInterval(slideNext, speed);
+
+    // On gère l'arrêt et la reprise
+    diapo.addEventListener("mouseover", stopTimer);
+    diapo.addEventListener("mouseout", startTimer);
+}
+
+/**
+ * Cette fonction fait défiler le diaporama vers la droite
+ */
+function slideNext(){
+    // On incrémente le compteur
+    compteur++;
+    elements.style.transition = transition+"ms linear";
+
+    let decal = -slideWidth * compteur;
+    elements.style.transform = `translateX(${decal}px)`;
+
+    const activeButton = document.querySelector('.carousel-indicators .active');
+    if (activeButton) {
+        activeButton.classList.remove('active');
+    }
+    const nextButton = document.querySelector(`#btn${compteur + 1}`);
+    if (nextButton) {
+        nextButton.classList.add('active');
+    }
+
+    // On attend la fin de la transition et on "rembobine" de façon cachée
+    setTimeout(function(){
+        if(compteur >= slides.length - 1){
+            compteur = 0;
+            elements.style.transition = "unset";
+            elements.style.transform = "translateX(0)";
+            const firstButton = document.querySelector('#btn1');
+            firstButton.classList.add('active');
+
+        }
+    }, transition);
+}
+
+/**
+ * Cette fonction fait défiler le diaporama vers la gauche
+ */
+function slidePrev(){
+    // On décrémente le compteur
+    compteur--;
+    elements.style.transition = transition+"ms linear";
+
+    const activeButton = document.querySelector('.carousel-indicators .active');
+    if (activeButton) {
+        activeButton.classList.remove('active');
+    }
+    const nextButton = document.querySelector(`#btn${compteur + 1}`);
+    if (nextButton) {
+        nextButton.classList.add('active');
+    }
+
+    if(compteur < 0){
+        compteur = slides.length - 1;
+        let decal = -slideWidth * compteur;
+        elements.style.transition = "unset";
+        elements.style.transform = `translateX(${decal}px)`;
+        setTimeout(slidePrev, 1);
+    }
+
+    let decal = -slideWidth * compteur;
+    elements.style.transform = `translateX(${decal}px)`;
+    
+}
+
+function stopTimer(){
+    clearInterval(timer);
+}
+
+function startTimer(){
+    timer = setInterval(slideNext, speed);
+}
+
+// Boutons du carroussel
+
+const buttonsCar = document.querySelectorAll('.carousel-indicators button');
+
+for (let button of buttonsCar) {
+  button.addEventListener('click', () => {
+    const index = parseInt(button.id.substring(3)) - 1;
+    // Si l'index est supérieur au compteur, on fait glisser vers la droite.
+    if (index > compteur) {
+      for (let i = -1; i < index - compteur; i++) {
+        slideNext();
+      }
+    }
+    // Si l'index est inférieur au compteur, on fait glisser vers la gauche.
+    else if (index < compteur) {
+      for (let i = -1; i < compteur - index; i++) {
+        slidePrev();
+      }
+    }
+  });
+}
+
+
+
 // Couleur des filtres
 
-function changerCouleurVert() {
-    document.querySelector(".btnTous").style.color = "#1D6154";
-    document.querySelector(".btnTous").style.backgroundColor = "white"
-}
-function changerCouleurBlanc() {
-    document.querySelector(".btnTous").style.color = "white";
-    document.querySelector(".btnTous").style.backgroundColor = "#864713"
-}
+var buttonsCouleurs = document.querySelectorAll('.filtres button');
 
-document.querySelectorAll('.btnConcert, .btnEntreprises, .btnMariages, .btnPortrait').forEach(button => { button.addEventListener("click", changerCouleurVert) })
-document.querySelector(".btnTous").addEventListener("click", changerCouleurBlanc)
+// boucle à travers les boutons et ajoute un événement de clic
+for (var i = 0; i < buttonsCouleurs.length; i++) {
+  buttonsCouleurs[i].addEventListener('click', function() {
+    
+    for (var j = 0; j < buttonsCouleurs.length; j++) {
+      buttonsCouleurs[j].style.color = "#864713";
+      buttonsCouleurs[j].style.backgroundColor = "white";
+    }
+    this.style.color = "white";
+    this.style.backgroundColor = "#864713";
+  });
+}
 
 
 //   Fonction filtre 
@@ -110,7 +248,6 @@ function afficherimage() {
             leftBtn.className = "modal-prev-btn"
             leftBtn.addEventListener('click', function(e) {
                 e.stopPropagation();
-                console.log(currentImageIndex)
                 if (currentImageIndex > 0) {
                     currentImageIndex--;
                 } else {
@@ -141,6 +278,4 @@ function afficherimage() {
         });
     }
 }
-
-
 afficherimage();
